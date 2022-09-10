2b2f02b3b3ff8a56c61195c4eb3b29678bd5a113
waterleaper <waterleaper8@gmail.com>
2022-09-10 14:15:05 +0900

    初コミット

---
+++ page-shuffle.php
@@ -0,0 +1,317 @@
+<?php
+
+/**
+ * Template Name: トピック班分け
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ */
+
+?>
+
+<head>
+  <meta charset="utf-8" />
+  <title>トピック班分け</title>
+  <style>
+    /*** The new CSS Reset - version 1.2.0 (last updated 23.7.2021) ***/
+
+    /* Remove all the styles of the "User-Agent-Stylesheet", except for the 'display' property */
+    *:where(:not(textarea, iframe, canvas, img, svg, video):not(svg *)) {
+      all: unset;
+      display: revert;
+    }
+
+    /* Preferred box-sizing value */
+    *,
+    *::before,
+    *::after {
+      box-sizing: border-box;
+    }
+
+    /* Remove list styles (bullets/numbers) */
+    ol,
+    ul {
+      list-style: none;
+    }
+
+    /* For images to not be able to exceed their container */
+    img {
+      max-width: 100%;
+    }
+
+    /* removes spacing between cells in tables */
+    table {
+      border-collapse: collapse;
+    }
+
+    /* revert the 'white-space' property for textarea elements on Safari */
+    textarea {
+      white-space: revert;
+    }
+
+    body {
+      width: 1200px;
+      margin: auto;
+    }
+
+    #overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.6);
+      z-index: 1;
+      display: none;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .fadeout {
+      animation: fadeOut 1s;
+      animation-fill-mode: both;
+    }
+
+    .spacer {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 24px 0;
+      width: 100%;
+    }
+
+    @keyframes fadeOut {
+      0% {
+        opacity: 1;
+      }
+
+      100% {
+        opacity: 0;
+      }
+    }
+
+    p#alert {
+      color: red;
+      display: none;
+    }
+
+    p#alert.show {
+      display: block;
+    }
+
+    .kuji-box {
+      width: 200px;
+      height: 200px;
+      background-image: url(../assets/kujibiki_box.png);
+      background-size: cover;
+      animation-name: rotateBox;
+      animation-duration: 1.5s;
+      animation-iteration-count: infinite;
+      animation-timing-function: step-start;
+    }
+
+    @keyframes rotateBox {
+      0% {
+        transform: rotate(-45deg);
+      }
+
+      50% {
+        transform: rotate(45deg);
+      }
+
+      100% {
+        transform: rotate(-45deg);
+      }
+    }
+
+    .kuji-man {
+      background-image: url(../assets/game_kuji_man.png);
+      background-size: cover;
+      width: 200px;
+      height: 200px;
+      position: absolute;
+      right: 0px;
+      top: 0;
+    }
+
+    h1 {
+      font-size: 2.4rem;
+      font-weight: bold;
+      position: relative;
+      color: #555;
+      margin-top: 48px;
+      text-align: center;
+    }
+
+    h2 {
+      font-weight: bold;
+      text-align: center;
+      padding-bottom: 6px;
+      font-size: 1.8rem;
+      color: #555;
+    }
+
+    small {
+      font-size: 0.8rem;
+    }
+
+    textarea {
+      font-size: 1.6rem;
+      border: none;
+      border: 1px solid #ccc;
+      border-radius: 6px;
+      color: #555;
+    }
+
+    .desc {
+      margin: 36px 0 24px;
+      text-align: center;
+    }
+
+    form {
+      margin-bottom: 18px;
+    }
+
+    button {
+      height: 48px;
+      padding: 14px 40px;
+      border-radius: 6px;
+      color: #ffffff;
+      background-color: #1a8cb3;
+      cursor: pointer;
+      transition: all 0.3s;
+      margin: auto;
+    }
+
+    button:hover {
+      background-color: white;
+      color: #000;
+      border: 1px solid #000;
+    }
+
+    input[type="file"] {
+      display: none;
+    }
+
+    input:hover {
+      opacity: 0.7;
+    }
+
+    .areas {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+    }
+
+    .area {
+      text-align: center;
+      margin-right: 24px;
+      margin-bottom: 12px;
+      padding: 12px 12px 24px;
+      border-radius: 12px;
+      width: 300px;
+    }
+
+    .area:last-child {
+      margin-right: 0;
+    }
+
+    .area_a {
+      background-color: #f9d6d2;
+    }
+
+    .area_b {
+      background-color: #d2f2f9;
+    }
+
+    .area_c {
+      background-color: #f2eca6;
+    }
+
+    @media screen and (max-width: 1200px) {
+      body {
+        width: 100%;
+      }
+    }
+  </style>
+</head>
+
+<div id="overlay">
+  <div class="kuji-box"></div>
+</div>
+<h1>
+  トピック チーム分け
+  <div class="kuji-man"></div>
+</h1>
+
+<div class="spacer">
+  <button onclick="reveal_namelist()">シャッフル</button>
+  <p id="alert">シャッフルに失敗しました</p>
+</div>
+
+<div class="areas">
+  <div class="area area_a">
+    <h2>A席</h2>
+    <textarea name="list_a" rows="12" cols="18" readonly></textarea><br />
+  </div>
+  <div class="area area_b">
+    <h2>B席</h2>
+    <textarea name="list_b" rows="12" cols="18" readonly></textarea><br />
+  </div>
+  <div class="area area_c">
+    <h2>C席</h2>
+    <textarea name="list_c" rows="12" cols="18" readonly></textarea>
+  </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+<script>
+  function reveal_namelist() {
+    const endpoint =
+      "https://script.google.com/a/macros/atgp-jobtra.jp/s/AKfycbxSSpxxvZKRDcRNwQ8dKcxTXlUqwzo8-WydNQTUfEt4dHrAcZBDoS1PuPeVWtG1yXA1rA/exec";
+    let namelistSplited;
+    $.ajax({
+        type: "GET",
+        url: endpoint,
+        dataType: "jsonp",
+        data: {
+          text: "hoge",
+        },
+      })
+      .done((out) => {
+        if (alert.classList.contains("show")) {
+          alert.classList.remove("show");
+        }
+        namelistSplited = out.data;
+
+        overlay.style.display = "flex";
+        document.body.style.overflow = "visible";
+        overlay.classList.remove("fadeout");
+
+        setTimeout(() => {
+          document.body.style.overflow = "hidden";
+          overlay.style.display = "none";
+          overlay.classList.add("fadeout");
+
+          const areas = document.querySelectorAll(".area");
+          areas[0].querySelector("textarea").value = namelistSplited[0];
+          areas[1].querySelector("textarea").value = namelistSplited[1];
+          areas[2].querySelector("textarea").value = namelistSplited[2];
+        }, 3000);
+      })
+      .fail(() => {
+        alert.classList.add("show");
+        namelistSplited = "error";
+      });
+  }
+
+  let alert = document.getElementById("alert");
+
+  function getNamelistSplited() {}
+</script>
+<?php wp_footer(); ?>
+
+<?php if (!defined('ABSPATH')) exit; ?>
---
+++ topic-shuffle.php
@@ -0,0 +1,473 @@
+<?php
+/*
+Plugin Name: TopicShuffle
+Description: トピックの班分け機能を追加します
+Version: 1.0
+Author: Keisuke Miura
+Author URI: https://next.waterleaper.net/
+License: GPLv2
+*/
+
+//=================================================
+// 管理画面に「とりあえずメニュー」を追加登録する
+//=================================================
+add_action('admin_menu', function () {
+
+  //---------------------------------
+  // メインメニュー①
+  //---------------------------------
+  add_menu_page(
+    'TopicShuffle' // ページのタイトルタグ<title>に表示されるテキスト
+    ,
+    'TopicShuffle'   // 左メニューとして表示されるテキスト
+    ,
+    'manage_categories'       // 必要な権限 manage_categories は通常 editor 以上に与えられた権限
+    ,
+    'topic_shuffle'        // 左メニューのスラッグ名 →URLのパラメータに使われる /wp-admin/admin.php?page=topic_shuffle
+    ,
+    'topic_shuffle_page_contents' // メニューページを表示する際に実行される関数(サブメニュー①の処理をする時はこの値は空にする)
+    ,
+    'dashicons-admin-users'       // メニューのアイコンを指定 https://developer.wordpress.org/resource/dashicons/#awards
+    ,
+    0                             // メニューが表示される位置のインデックス(0が先頭) 5=投稿,10=メディア,20=固定ページ,25=コメント,60=テーマ,65=プラグイン,70=ユーザー,75=ツール,80=設定
+  );
+});
+
+//実行するよ！
+$topic_shuffleObj = new Topic_Shuffle();
+
+class Topic_Shuffle
+{
+
+  public function __construct()
+  {
+    global $topic_shuffle_version;
+    $topic_shuffle_version = '1.0';
+
+    //初期化
+    $this->admin_init();
+  }
+
+
+  public function admin_init()
+  {
+    register_activation_hook(__FILE__, array($this, 'topic_shuffle_install')); // プラグイン有効化時にDB作成
+    register_activation_hook(__FILE__, array($this, 'topic_shuffle_install_data')); //プラグイン有効化時にDB初期化
+    // register_deactivation_hook(__FILE__, array($this, 'topic_shuffle_delete_data')); //プラグイン停止時に実行する関数を登録
+
+    add_action('plugins_loaded', array($this, 'myplugin_update_db_check')); //プラグイン更新時のDB更新チェック
+    add_shortcode("NAMES-SHOW", array($this, 'names_start'));
+    add_shortcode("NGCOMBOS-SHOW", array($this, 'ngcombos_start'));
+    add_shortcode("NAMES-OPTION-SHOW", array($this, 'names_option_start'));
+    add_filter('widget_text', 'do_shortcode'); //ウィジェットでショートコードが使用できるようにする。
+  }
+
+  // 名簿管理機能 =====================================================
+
+  public function names_start()
+  {
+    $this->names_show();
+  }
+
+  public function ngcombos_start()
+  {
+    $this->ngcombos_show();
+  }
+  public function names_option_start()
+  {
+    $this->names_option_show();
+  }
+
+  public function names_show()
+  {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'topic_names';
+
+    $sql = "SELECT id,name FROM {$table_name};";
+    $results = $wpdb->get_results($sql);
+    foreach ($results as $row) {
+      echo "
+        <form method='post'>
+          <tr>
+            <input type='hidden' name='id' value='{$row->id}'>
+            <td>{$row->id}</td>
+            <td>{$row->name}</td>
+            <td>
+                <button type='submit' name='delete'>削除する</button>
+            </td>
+          <tr>
+        </form>
+      ";
+    }
+  }
+
+  public function names_option_show()
+  {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'topic_names';
+
+    $sql = "SELECT id,name FROM {$table_name};";
+    $results = $wpdb->get_results($sql);
+    foreach ($results as $row) {
+      echo "
+        <option value='{$row->name}'>{$row->name}</option>
+      ";
+    }
+  }
+
+  public function ngcombos_show()
+  {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'topic_ngcombos';
+
+    $sql = "SELECT id,name1,name2 FROM {$table_name};";
+    $results = $wpdb->get_results($sql);
+    foreach ($results as $row) {
+      echo "
+        <form method='post'>
+          <tr>
+            <input type='hidden' name='ng-id' value='{$row->id}'>
+            <td>{$row->id}</td>
+            <td>{$row->name1}</td>
+            <td>{$row->name2}</td>
+            <td>
+                <button type='submit' name='ng-delete'>削除する</button>
+            </td>
+          <tr>
+        </form>
+      ";
+    }
+  }
+  // 名簿管理機能 ここまで =====================================================
+
+
+  //DBの作成 ※プラグイン有効時
+  function topic_shuffle_install()
+  {
+    $plugin_dir = plugin_dir_path(__FILE__) . '/page-shuffle.php';
+    $theme_dir = get_stylesheet_directory() . '/page-shuffle.php';
+    copy($plugin_dir, $theme_dir);
+    create_pages_and_setting();
+
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'topic_names';
+    $table_name2 = $wpdb->prefix . 'topic_ngcombos';
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+                id mediumint(9) NOT NULL AUTO_INCREMENT,
+                name varchar(16) NOT NULL,
+                UNIQUE KEY id (id),
+                PRIMARY KEY (name)
+                )
+                {$charset_collate}, ENGINE=InnoDB;";
+
+    $sql2 = "CREATE TABLE $table_name2 (
+                id mediumint(9) NOT NULL AUTO_INCREMENT,
+                name1 varchar(16) NOT NULL,
+                name2 varchar(16) NOT NULL,
+                UNIQUE KEY id (id),
+                FOREIGN KEY (name1) REFERENCES wp_topic_names(name),
+                FOREIGN KEY (name2) REFERENCES wp_topic_names(name)
+                )
+                {$charset_collate}, ENGINE=InnoDB;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+    dbDelta($sql2);
+
+    add_option('topic_shuffle_version', $topic_shuffle_version);
+  }
+
+  //DBの初期化 ※プラグイン有効時
+  function topic_shuffle_install_data()
+  {
+
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'topic_names';
+
+    $count_init = 0;
+
+    $wpdb->insert(
+      $table_name,
+      array(
+        'time' => current_time('mysql'),
+        'count' => $count_init,
+      )
+    );
+  }
+
+
+  //DB更新チェック ※プラグイン更新
+  function myplugin_update_db_check()
+  {
+    if (get_site_option('topic_shuffle_version') != $topic_shuffle_version) {
+      $this->topic_shuffle_install();
+    }
+  }
+
+
+  //DBの削除 ※プラグイン停止時
+  function topic_shuffle_delete_data()
+  {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'topic_names';
+    $table_name2 = $wpdb->prefix . 'topic_ngcombos';
+
+    delete_option('topic_shuffle_version');
+    $sql = "DROP TABLE {$table_name};";
+    $sql2 = "DROP TABLE {$table_name2};";
+    $wpdb->query($sql);
+    $wpdb->query($sql2);
+  }
+} //Topic_Shuffle
+
+function add_name($text)
+{
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'topic_names';
+  $wpdb->insert(
+    $table_name,
+    array(
+      'name' => $text,
+    )
+  );
+}
+function delete_name($id)
+{
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'topic_names';
+  $wpdb->delete(
+    $table_name,
+    array(
+      'id' => $id,
+    )
+  );
+}
+function add_ngcombos($name1, $name2)
+{
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'topic_ngcombos';
+  $wpdb->insert(
+    $table_name,
+    array(
+      'name1' => $name1,
+      'name2' => $name2,
+    )
+  );
+}
+function delete_ngcombos($id)
+{
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'topic_ngcombos';
+  $wpdb->delete(
+    $table_name,
+    array(
+      'id' => $id,
+    )
+  );
+}
+
+function nowUrl()
+{
+  $url = '';
+  if (isset($_SERVER['HTTPS'])) {
+    $url .= 'https://';
+  } else {
+    $url .= 'http://';
+  }
+  $url .= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  return $url;
+}
+
+function get_names_array()
+{
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'topic_names';
+
+  $sql = "SELECT id,name FROM {$table_name};";
+  $results = $wpdb->get_results($sql);
+  $arr = array();
+  foreach ($results as $row) {
+    array_push($arr, $row->name);
+  }
+  return $arr;
+}
+function get_ngcombos_array()
+{
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'topic_ngcombos';
+
+  $sql = "SELECT name1,name2 FROM {$table_name};";
+  $results = $wpdb->get_results($sql);
+  $arr = array();
+  foreach ($results as $row) {
+    $arr2 = array();
+    array_push($arr2, $row->name1);
+    array_push($arr2, $row->name2);
+    asort($arr2);
+    array_push($arr, implode($arr2));
+  }
+  return $arr;
+}
+function create_pages_and_setting()
+{
+  //$pages_array[] = array('title'=>'ページタイトル', 'name'=>'スラッグ', 'parent'=>'親スラッグ');
+  //例としてお問い合わせページを入力(親ページなし)
+  $pages_array[] = array(
+    'title' => 'トピック班分け',
+    'name' => 'shuffle',
+    'parent' => '',
+    'template' => 'page-shuffle.php'
+  );
+  foreach ($pages_array as $value) {
+    setting_pages($value);
+  }
+}
+function setting_pages($val)
+{
+  //親ページ判別
+  if (!empty($val['parent'])) {
+    $parent_id = get_page_by_path($val['parent']);
+    $parent_id = $parent_id->ID;
+    $page_slug = $val['parent'] . "/" . $val['name'];
+  } else {
+    $parent_id = "";
+    $page_slug = $val['name'];
+  }
+  if (empty(get_page_by_path($page_slug))) {
+    //固定ページがなければ作成
+    $insert_id = wp_insert_post(
+      array(
+        'post_title'   => $val['title'],
+        'post_name'    => $val['name'],
+        'page_template' => $val['template'],
+        'post_status'  => 'publish',
+        'post_type'    => 'page',
+        'post_parent'  => $parent_id,
+      )
+    );
+  } else {
+    //固定ページがすでにあれば更新
+    $page_obj = get_page_by_path($page_slug);
+    $page_id = $page_obj->ID;
+    $base_post = array(
+      'ID'           => $page_id,
+      'post_title'   => $val['title'],
+      'post_name'    => $val['name'],
+      'page_template' => $val['template'],
+    );
+    wp_update_post($base_post);
+  }
+}
+
+//=================================================
+// シャッフル機能
+//=================================================
+
+//=================================================
+// メインメニューページ内容の表示・更新処理
+//=================================================
+function topic_shuffle_page_contents()
+{
+  $names = get_names_array();
+  $ngcombos = get_ngcombos_array();
+  // ↑ DBの各種設定
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!empty($_POST['name']) && in_array($_POST['name'], $names)) {
+      echo  '<p style="color:red;">※同じ名前は追加できません。</p>';
+    } else if (!empty($_POST['name'])) {
+      add_name($_POST['name']);
+    } else if (isset($_POST['delete'])) {
+      delete_name($_POST['id']);
+    } else if (isset($_POST['ng-delete'])) {
+      delete_ngcombos($_POST['ng-id']);
+    } else if (!empty($_POST['ng-name1']) && !empty($_POST['ng-name2'])) {
+      $_arr = array($_POST['ng-name1'], $_POST['ng-name2']);
+      asort($_arr);
+      $ngcombo = implode($_arr);
+      if (in_array($ngcombo, $ngcombos)) {
+        echo '<p style="color:red;">※同じ組み合わせは追加できません。</p>';
+      } else if ($_POST['ng-name1'] == $_POST['ng-name2']) {
+        echo '<p style="color:red;">※同じ名前を組み合わせることはできません。</p>';
+      } else {
+        add_ngcombos($_POST['ng-name1'], $_POST['ng-name2']);
+      }
+    } else {
+      header("Location: " . nowUrl());
+    }
+  }
+  //---------------------------------
+  // HTML表示
+  //---------------------------------
+  echo <<<EOF
+    <div class="wrap">
+      <style>
+        table {
+          margin-top: 10px;
+          border-spacing: 0px 0px;
+        }
+        td, th {
+          padding: 3px 6px;
+        }
+        table, td, th {
+          border: 1px #969696 solid;
+        }
+      </style>
+      <h1 style="font-weight: bold;">TopicShuffle</h1>
+      <a href="/shuffle">トピック班分けページへ</a>
+      <p>
+        トピック班分けアプリの管理ページです。
+      </p>
+      <h3>名前リスト</h3>
+      <form method="post" class="name-input-wrapper">
+        <input type="text" id="name" name="name" required maxlength="16" size="16">
+        <button type="submit">名前を追加</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>名前</th>
+            <th>削除</th>
+          </tr>
+        </thead>
+        <tbody>
+  EOF;
+  echo do_shortcode('[NAMES-SHOW]');
+  echo <<<EOF
+        </tbody>
+      </table>
+      <h3>NG組み合わせリスト</h3>
+      <form method="post" class="ngcombos-input-wrapper">
+        <select type="text" id="ng-name1" name="ng-name1" required>
+  EOF;
+  echo do_shortcode('[NAMES-OPTION-SHOW]');
+  echo <<<EOF
+        </select>
+        <select type="text" id="ng-name2" name="ng-name2" required>
+  EOF;
+  echo do_shortcode('[NAMES-OPTION-SHOW]');
+  echo <<<EOF
+        </select>
+        <button type="submit">組み合わせを追加</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>名前1</th>
+            <th>名前2</th>
+            <th>削除</th>
+          </tr>
+        </thead>
+        <tbody>
+  EOF;
+  echo do_shortcode('[NGCOMBOS-SHOW]');
+  echo <<<EOF
+    </div>
+  EOF;
+}
+
+if (!defined('ABSPATH')) exit;
